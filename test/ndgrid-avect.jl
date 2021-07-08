572164a02ddf5553ae4844bb520e147fa45f5291
Jeff Fessler <fessler@umich.edu>
2021-07-08 14:50:53 -0400

    test

---
+++ test/ndgrid-avect.jl
@@ -0,0 +1,24 @@
+# ndgrid-avect.jl
+
+using LazyGrids: ndgrid, ndgrid_array, GridAV, GridUR
+using Test: @test, @testset, @test_throws, @inferred
+
+@testset "avect" begin
+    (x, y, z) = ([:a,:b,:c], LinRange(0,1,4), 5:9)
+    (xa, ya, za) = @inferred ndgrid_array(x, y, z)
+    (xl, yl, zl) = @inferred ndgrid(x, y, z)
+
+    @test eltype(xl) === eltype(x)
+    @test eltype(yl) === eltype(y)
+    @test eltype(zl) === eltype(z)
+    @test size(xl) === (length(x), length(y), length(z))
+    @test xl isa GridAV
+    @test yl isa GridAR
+    @test zl isa GridUR
+    @test xl == xa
+    @test yl ≈ ya
+    @test zl == za
+    @test xl[2,3,4] == xa[2,3,4] == x[2]
+    @test yl[2,3,4] ≈ ya[2,3,4] ≈ y[3]
+    @test zl[2,3,4] == za[2,3,4] == z[4]
+end
