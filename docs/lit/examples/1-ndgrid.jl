343442d0622d178c64ff69ad130d2b8ad698eff9
Jeff Fessler <fessler@umich.edu>
2021-07-08 09:42:02 -0400

    lit

--- docs/lit/examples/1-ndgrid.jl
+++ docs/lit/examples/1-ndgrid.jl
@@ -28,7 +28,7 @@ using BenchmarkTools
 
 Δ = 1/2^10
 x = range(-1, stop=1, step=Δ)
-y = x
+y = copy(x)
 
 circle(x::Real,y::Real) = x^2 + y^2 < 1
 circle(xy::NTuple{2}) = abs2(xy[1]) + abs2(xy[2]) < 1
@@ -67,6 +67,19 @@ method_ndgrid2(xg, yg) = sum(circle.(xg,yg)) * Δ^2
 (xg, yg) = ndgrid(x, y)
 @assert method_ndgrid2(xg, yg) ≈ area0
 #@btime method_ndgrid2($xg, $yg) # 5.3 ms (7 allocations: 516.92 KiB)
+
+
+function method0g(xg,yg)
+	sum = 0.0
+	for i in 1:length(xg)
+	#	sum += circle(xg[i],yg[i])
+		tmp = (xg[i],yg[i])
+		sum += circle(tmp...)
+	end
+	return sum * Δ^2
+end
+@assert method0g(xg, yg) ≈ area0
+#@btime method0g($xg, $yg) # 5.960 ms (3 allocations: 48 bytes)
 
 
 # Using `zip` can avoid the "extra" memory beyond the grids.
@@ -113,6 +126,7 @@ method6(x,y) = sum(circle, Iterators.product(x, y)) * Δ^2
 #@assert yl == yg
 sizeof(xg), sizeof(xl)
 
+
 method7(xl,yl) = sum(circle, zip(xl,yl)) * Δ^2
 @assert method7(xl,yl) ≈ area0
 #@btime method7(xl,yl) # 23.7 ms (3 allocations: 48 bytes)
@@ -122,7 +136,8 @@ method7(xl,yl) = sum(circle, zip(xl,yl)) * Δ^2
 # so let's move on to a 3D example: finding volume of a sphere.
 
 sphere(x::Real,y::Real,z::Real) = x^2 + y^2 + z^2 < 1
-sphere(r::NTuple{3}) = abs2(r[1]) + abs2(r[2]) + abs2(r[3]) < 1
+#sphere(r::NTuple{3}) = abs2(r[1]) + abs2(r[2]) + abs2(r[3]) < 1
+sphere(r::NTuple) = sum(abs2, r) < 1
 
 
 # Storing three 3D arrays of size 2049^3 Float64 would take 192GB,
@@ -144,23 +159,81 @@ sphere1(x,y,z,Δ) = sum(sphere.(
 		repeat(reshape(y, (1, :, 1)), length(x), 1, length(z)),
 		repeat(reshape(z, (1, 1, :)), length(x), length(y), 1),
 	)) * Δ^3
-#@time vol0 = sphere1(xc,yc,zc,Δc) # 3sec 3GiB, roughly (4/3)π
+vol0 = sphere1(xc,yc,zc,Δc) # roughly (4/3)π
+#@time sphere1(xc,yc,zc,Δc) # 2.6sec 3.0GiB
 
 
-# `ndgrid` is surprisingly fast for the coarse grid
-(xgc, ygc, zgc) = ndgrid(xc, yc, zc)
-sphere3(xg, yg, zg, Δ) = sum(sphere, zip(xg,yg,zg)) * Δ^3
-@time sphere3(xgc, ygc, zgc, Δc) # 0.23 sec, 2.6 MiB
+# The sum part of `ndgrid` is surprisingly fast for the coarse grid
+# but creating the grid itself is quite slow.
+(xgc, ygc, zgc) = ndgrid(xc, yc, zc) # warm-up
+#@time (xgc, ygc, zgc) = ndgrid(xc, yc, zc) # 2.6 sec 3.0GiB
 
-# `ndgrid_lazy` todo
+sphere3(xx, yy, zz, Δ) = sum(sphere, zip(xx,yy,zz)) * Δ^3
+@assert sphere3(xgc, ygc, zgc, Δc) ≈ vol0
+#@time sphere3(xgc, ygc, zgc, Δc) # 0.19 sec, 16 byte
+
+# `ndgrid_lazy`
 (xlc, ylc, zlc) = ndgrid_lazy(xc, yc, zc)
+#@btime (xlc, ylc, zlc) = ndgrid_lazy($xc, $yc, $zc); # 0.03ns 0 bytes (!)
+
+# uses almost no memory:
 sizeof( (xlc, ylc, zlc) )
-@time sphere3(xlc, ylc, zlc, Δc) # 1.59 sec, 38 MiB todo: why so much?
+
+# and is faster than the combined time of `ndgrid` and the volume summation:
+@assert sphere3(xlc, ylc, zlc, Δc) ≈ vol0
+#@time sphere3(xlc, ylc, zlc, Δc) # 1.4 sec, 16 byte
+
+# Importantly, with a lazy ndgrid now we can return to the fine scale;
+# it takes a minute or two, but it is feasible because of the low memory.
+z = copy(x)
+#@btime ndgrid_lazy($x, $y, $z); # 0.03ns 0 bytes (!)
+(xlf, ylf, zlf) = ndgrid_lazy(x, y, z)
+
+@time sphere3(xlf, ylf, zlf, Δ) # 93 sec, 16 bytes
+throw(78)
+
+
+# todo
+# With a lazy grid, now we can explore higher-dimensional spheres
+# nope, too slow even with coarse grid :(
+# π^2/2, sum(sphere, zip(ndgrid_lazy(xc,xc,xc,xc)...)) * Δ^4
 
 #=
 
 [CartesianIndices](https://julialang.org/blog/2016/02/iteration)
 
-# ### Methods
+# ### Junk
+
+# todo delete this
+	(xg, yg) = ndgrid(x, y)
+#mapreduce(abs2, +, xg; init=0.0)
+method_map1(xg,yg) = mapreduce(circle, +, xg, yg; init=0.0) * Δ^2
+@show method_map1(xg,yg)
+#@btime method_map1($xg,$yg) # 8.8 ms (5 allocations: 4.00 MiB)
+
+method7_map(xx,yy) = mapreduce(circle, +, xx, yy; init=0.0) * Δ^2
+@assert method7_map(xl,yl) ≈ area0
+@btime method7_map(xl,yl) # 27.9 ms (5 allocations: 4.00 MiB)
+
+function method0l(xx,yy)
+	sum = 0.0
+	for i in 1:length(xx)
+	 	sum += circle(xx[i],yy[i])
+	end
+	return sum * Δ^2
+end
+@assert method0l(xl, yl) ≈ area0
+#@btime method0l($xl, $yl) # 48.000 ms (3 allocations: 48 bytes)
+throw("0l")
+
+function sphere0l(xx,yy,zz,Δ)
+	sum = 0.0
+	for i in 1:length(xx)
+	 	sum += sphere(xx[i],yy[i],zz[i])
+	end
+	return sum * Δ^3
+end
+@time @assert sphere0l(xlc, ylc, zlc, Δc) ≈ vol0 # 3.3 sec, 16 bytes
+#@btime sphere0l($xlc, $ylc, $zlc, Δc)
 
 =#
