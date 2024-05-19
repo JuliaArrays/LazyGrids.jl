5f279fc730f96995b7a55ce81a9270930daec686
Jeff Fessler <fessler@umich.edu>
2024-05-19 08:48:41 -0400

    Test ambiguities

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,15 @@
 # runtests.jl
 
+using LazyGrids: LazyGrids
+using Test: @test, @testset, detect_ambiguities
+
 include("aqua.jl")
 include("ndgrid-oneto.jl")
 include("ndgrid-unitr.jl")
 include("ndgrid-range.jl")
 include("ndgrid-avect.jl")
 include("timer.jl")
+
+@testset "ambiguities" begin
+    @test isempty(detect_ambiguities(LazyGrids))
+end
