01693454d6900c55c6a5bc29339a7bf0b348d92a
Jeff Fessler <fessler@umich.edu>
2025-12-28 15:43:17 -0500

    Add warning about private functions

--- src/timer.jl
+++ src/timer.jl
@@ -4,11 +4,14 @@ Utilities used in the Literate example
 =#
 
 using Statistics: median
-using Base: gc_num, gc_alloc_count, GC_Diff, timev_print
-#using Base: cumulative_compile_time_ns_before, cumulative_compile_time_ns_after
+using Base: gc_num, gc_alloc_count, GC_Diff # caution: private!
 
 
-# cf https://github.com/JuliaLang/julia/blob/6aaedecc447e3d8226d5027fb13d0c3cbfbfea2a/base/timing.jl#L327-L362
+#=
+cf macro timed at
+https://github.com/JuliaLang/julia/blob/master/base/timing.jl
+https://github.com/JuliaLang/julia/blob/6aaedecc447e3d8226d5027fb13d0c3cbfbfea2a/base/timing.jl#L327-L362
+=#
 
 """
     @timeo
@@ -19,14 +22,11 @@ macro timeo(ex) # @timeo macro to show only the timing, not results
     quote
         while false; end # compiler heuristic:
         local stats = gc_num()
-#       local compile_time = cumulative_compile_time_ns_before()
         local elapsedtime = time_ns()
         local val = $(esc(ex))
         elapsedtime = time_ns() - elapsedtime
-#       compile_time = cumulative_compile_time_ns_after() - compile_time
         local diff = GC_Diff(gc_num(), stats)
         local allocs = gc_alloc_count(diff)
-#       timev_print(elapsedtime, diff, compile_elapsedtime)
 #       time_print(elapsedtime, diff.allocd, diff.total_time, allocs, compile_time, true)
         string(
             "time=", round(elapsedtime/10^6, digits=1), # time in ms
